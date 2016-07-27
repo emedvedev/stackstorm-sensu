9f2f4b830d5a885e16f04155afed2ce63d4a65d2
Edward Medvedev <edward.medvedev@gmail.com>
2016-07-27 15:12:31 +0700

    Transfer from st2contrib: Sensu

---
+++ actions/health.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from lib import sensu
+import argparse
+
+parser = argparse.ArgumentParser(description='Sensu Health Check')
+
+parser.add_argument('--consumers', nargs='?', default=False)
+parser.add_argument('--messages', nargs='?', default=False)
+args = parser.parse_args()
+
+status = sensu.Status('config.yaml')
+
+print(status.health(args.consumers, args.messages))
